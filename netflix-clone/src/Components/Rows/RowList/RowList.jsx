26e645103b0d11a6b7444b1350dfc772747afae7
jossi2025 <jossihabtetsion10@gmail.com>
2025-02-25 17:22:39 +0200

    requests.js

--- netflix-clone/src/Components/Rows/RowList/RowList.jsx
+++ netflix-clone/src/Components/Rows/RowList/RowList.jsx
@@ -1,26 +1,25 @@
-import React from 'react'
-import Row from "../Row/Row"
-import requests from '../../../utils/requests'
-
-
+import React from "react";
+import Row from "../Row/Row";
+import requests from "../../../utils/requests";
 
 const RowList = () => {
-    return (
+  return (
     <>
-    <Row 
+      <Row
         title="Netflix Originals"
-        fetchUrl={requests.fetchNetflixOriginals} isLargeRow={true} />
-        <Row title="Trending" fetchUrl={requests.fetchTrending} />
-        <Row title="TopRated Movies" fetchUrl={requests.fetchTopRatedMovies} />
-        <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
-        <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
-        <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
-        <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
-              <Row title="Tv Show" fetchUrl={requests.fetchTvShow} />
-        <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} /> 
-  
-      </>
-    )
-}
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow={true}
+      />
+      <Row title="Trending" fetchUrl={requests.fetchTrending} />
+      <Row title="TopRated Movies" fetchUrl={requests.fetchTopRatedMovies} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Tv Show" fetchUrl={requests.fetchTvShow} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+    </>
+  );
+};
 
-export default RowList
+export default RowList;
--- netflix-clone/src/utils/requests.js
+++ netflix-clone/src/utils/requests.js
@@ -4,11 +4,11 @@ const request  = {
     fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213` ,
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US` ,
     fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US` ,
-    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&width_genres=28` ,
-    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&width_genres=35` ,
-    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&width_genres=27` ,
-    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&lwidth_genres=10749` ,
-    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&width_genres=99` ,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28` ,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35` ,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27` ,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749` ,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99` ,
     fetchTvShow: `tv/popular?api_key=${API_KEY}&language=en-US` ,
 };
 
